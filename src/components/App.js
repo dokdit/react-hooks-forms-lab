198339269261c56b27549f9ec42d9eeae0854edd
AK <akueiminyang@cmail.carleton.ca>
2023-06-19 17:11:28 -0400

    Lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -11,17 +11,12 @@ function App() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
-  function addItem(newItem) {
-    setItems((prevItems) => [...prevItems, newItem]);
-  }
-
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} onItemAdd={addItem} />
+      <ShoppingList items={items} />
     </div>
   );
 }
-
 
 export default App;
